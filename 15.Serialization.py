f902f103a92107334efd64c5b6e1759506430d1f
kesen <317331820@qq.com>
2015-04-09 15:50:14 +0800

    modify 15.Serialization.py 添加了XML和HTML解析内容

--- 15.Serialization.py
+++ 15.Serialization.py
@@ -65,3 +65,84 @@ def dict2student(d):
 json_str = '{"age": 20, "score": 88, "name": "Bob"}'
 print json.loads(json_str, object_hook = dict2student)
 
+
+print u'-------------------------5. XML------------------------------'
+#操作XML有两种方法：DOM和SAX。DOM会把整个XML读入内存，解析为树，因此占用内存大，解析慢，
+#优点是可以任意遍历树的节点。SAX是流模式，边读边解析，占用内存小，解析快，缺点是我们需要自己处理事件。
+
+#正常情况下，优先考虑SAX，因为DOM实在太占内存。
+#在Python中使用SAX解析XML非常简洁，通常我们关心的事件是start_element，end_element和char_data，准备好这3个函数，然后就可以解析xml了。
+
+#当SAX解析器读到一个节点:<a href="/">python</a> 时，会产生3个事件：
+
+#1. start_element事件，在读取<a href="/">时；
+#2. char_data事件，在读取python时；
+#3. end_element事件，在读取</a>时。
+
+from xml.parsers.expat import ParserCreate
+
+class DefaultSaxHandler(object):
+	def start_element(self, name, attrs):
+		print('sax:start_element: %s, attrs: %s' % (name, str(attrs)))
+
+	def char_data(self, text):
+		print('sax:char_data: %s' % text)
+
+	def end_element(self, name):
+		print('sax:end_element: %s' % name)
+
+xml = r'''<?xml version="1.0"?>
+<ol>
+    <li><a href="/python">Python</a></li>
+    <li><a href="/ruby">Ruby</a></li>
+</ol>
+'''
+
+handler = DefaultSaxHandler()
+parser = ParserCreate()
+parser.returns_unicode = True
+parser.StartElementHandler = handler.start_element
+parser.CharacterDataHandler = handler.char_data
+parser.EndElementHandler = handler.end_element
+
+parser.Parse(xml)
+
+#当设置returns_unicode为True时，返回的所有element名称和char_data都是unicode，处理国际化更方便。
+#需要注意的是读取一大段字符串时，CharacterDataHandler可能被多次调用，所以需要自己保存起来，在EndElementHandler里面再合并。
+
+print u'------------------------- 6. HTMLParser ------------------------------'
+# HTML本质上是XML的子集，但是HTML的语法没有XML那么严格，所以不能用标准的DOM或SAX来解析HTML。
+# 好在Python提供了HTMLParser来非常方便地解析HTML，只需简单几行代码：
+
+from HTMLParser import HTMLParser
+from htmlentitydefs import name2codepoint
+
+class MyHTMLParser(HTMLParser):
+
+    def handle_starttag(self, tag, attrs):
+        print('<%s>' % tag)
+
+    def handle_endtag(self, tag):
+        print('</%s>' % tag)
+
+    def handle_startendtag(self, tag, attrs):
+        print('<%s/>' % tag)
+
+    def handle_data(self, data):
+        print('data')
+
+    def handle_comment(self, data):
+        print('<!-- -->')
+
+    def handle_entityref(self, name):
+        print('&%s;' % name)
+
+    def handle_charref(self, name):
+        print('&#%s;' % name)
+
+parser = MyHTMLParser()
+parser.feed('<html><head></head><body><p>Some <a href=\"#\">html</a> tutorial...<br>END</p></body></html>')
+
+# feed()方法可以多次调用，也就是不一定一次把整个HTML字符串都塞进去，可以一部分一部分塞进去。
+
+# 特殊字符有两种，一种是英文表示的&nbsp;，一种是数字表示的&#1234;，这两种字符都可以通过Parser解析出来
