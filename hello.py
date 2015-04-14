b9d3c88b2d70765caa6257d3bc737efe6b56cc5b
kesen <317331820@qq.com>
2015-04-14 11:23:49 +0800

    Create function of dataPathHere

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python  
 # -*- coding: utf-8 -*-
+import os
 
 class Hello(object):
 	def hello(self, name = 'world'):
 		print ('hello, %s.' % name)
+
+# 返回./testdir/filename
+def dataPathHere(filename): 
+	datapath = os.path.join(os.path.abspath('.'), 'testdir') # ./testdir
+	filePath = os.path.join(datapath, filename) # ./testdir/filename
+	return filePath
+
+print dataPathHere('dsfd')
